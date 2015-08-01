7bf15d71e8989c8f9aba55b76ff49fce11ceed8f
Steve Mynott <steve.mynott@gmail.com>
2015-08-01 16:58:18 +0100

    import HTML.pm

---
+++ lib/App/Pl6anet/HTML.pm
@@ -0,0 +1,19 @@
+use v6;
+use Template::Mojo;
+
+role App::Pl6anet::HTML {
+
+    #    subset File of Str where -> $x { so $x && $x.IO.e };
+    has Str  $.tmpl;
+    has Hash  %.data;
+
+    method render {
+        my $f = slurp $.tmpl;
+        my $t = Template::Mojo.new($f);
+        my $html =  $t.render( %.data );
+        return $html;
+    }
+
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6
